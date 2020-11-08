4d649ac54dce323d27812dead8e31d518913edc9
lethienhongson96 <66103162+lethienhongson96@users.noreply.github.com>
2020-11-08 21:13:36 +0700

    HongSon-->Get Swagger for offroadvietnamAPI

--- OffRoadVietNamAPI/OffRoadVietNamAPI/Startup.cs
+++ OffRoadVietNamAPI/OffRoadVietNamAPI/Startup.cs
@@ -26,6 +26,7 @@ namespace OffRoadVietNamAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +36,13 @@ namespace OffRoadVietNamAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "OffRoadVietNam API");
+            });
+            app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
